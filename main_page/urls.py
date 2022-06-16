75ee2831337b7a6608689aec5d3078e8d2568753
amuriks7 <amuriks7@gmail.com>
2022-06-16 16:41:18 +0300

    add new page typography

--- main_page/urls.py
+++ main_page/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import main_page
-
+from .views import main_page, render_typography_page
 
 app_name = 'main_page'
 
 urlpatterns = [
     path('', main_page, name='main_page_view'),
+    path('typography/', render_typography_page, name='typography_view'),
 ]
--- main_page/views.py
+++ main_page/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from .models import Category, Dish
-from .forms import UserReservationForm
 
 
 def main_page(request):
-
     categories = Category.objects.filter(is_visible=True).order_by('position')
     dishes = Dish.objects.filter(is_visible=True).order_by('position')
 
     return render(request, 'main.html', context={'categories': categories, 'dishes': dishes})
+
+
+def render_typography_page(request):
+    return render(request, 'typography.html', context={})
