9d4ef11a0f47dca562e96ca3346df4f6f36176a4
amuriks7 <amuriks7@gmail.com>
2022-06-25 14:02:04 +0300

    add files for heroku

--- main_page/urls.py
+++ main_page/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+
+from django.conf.urls.static import settings
+from django.conf.urls.static import static
+
 from .views import main_page, render_typography_page, about_new
 
 app_name = 'main_page'
@@ -7,5 +11,7 @@ urlpatterns = [
     path('', main_page, name='main_page_view'),
     path('typography/', render_typography_page, name='typography_view'),
     path('about/', about_new, name='about_view'),
-
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
